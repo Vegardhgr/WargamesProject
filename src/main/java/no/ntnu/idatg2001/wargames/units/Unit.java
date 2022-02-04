d437f2510b38d8d18431ff95271536ac4a48ba36
Vegardhgr <vegard.groder@gmail.com>
2022-02-04 17:36:37 +0100

    Done with the abstract Unit class

---
+++ src/main/java/no/ntnu/idatg2001/wargames/units/Unit.java
@@ -0,0 +1,76 @@
+package no.ntnu.idatg2001.wargames.units;
+
+public abstract class Unit {
+    /**
+     * Fields that all units have in common
+     */
+    private final String name;
+    private int health;
+    private int attack;
+    private int armor;
+
+    /**
+     * Initializes all the fields
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    /**
+     * The opponent's health after getting attacked
+     * @param opponent
+     */
+    public void attack(Unit opponent) {
+        int health = opponent.getHealth() - (attack + getAttackBonus()) + (opponent.armor + opponent.getResistBonus());
+        opponent.setHealth(health);
+    }
+
+    /**
+     * Returns the name of the character
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the health of the character
+     * @return health
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * Returns attack value
+     * @return attack
+     */
+    public int getAttack() {
+        return attack;
+    }
+
+    /**
+     * Returns the armor value
+     * @return armor
+     */
+    public int getArmor() {
+        return armor;
+    }
+
+    /**
+     * Sets the character's new health
+     * @param health
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public abstract int getAttackBonus();
+    public abstract int getResistBonus();
+}
