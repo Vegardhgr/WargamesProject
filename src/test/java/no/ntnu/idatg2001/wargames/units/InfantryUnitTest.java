bff3fcf68859659b672b09f3637f82e43f1d2b42
Vegard <vegarhgr@stud.ntnu.no>
2022-02-10 15:51:02 +0100

    Completed the InfatryUnit and the InfantryUnitTest classes

---
+++ src/test/java/no/ntnu/idatg2001/wargames/units/InfantryUnitTest.java
@@ -0,0 +1,39 @@
+package no.ntnu.idatg2001.wargames.units;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * A test class for the InfantryUnit class.
+ *
+ * @author Vegard Grøder
+ * @version 10.02.2022
+ */
+class InfantryUnitTest {
+    // A object made global
+    InfantryUnit barbarian;
+
+    @BeforeEach
+    void setUp() {
+        // Initializes the barbarian object
+        barbarian = new InfantryUnit("Barbarian", 12);
+    }
+
+    /**
+     * Tests the attack bonus method
+     */
+    @Test
+    void getAttackBonus() {
+        assertEquals(2, barbarian.getAttackBonus());
+    }
+
+    /**
+     * Tests the resist bonus method
+     */
+    @Test
+    void getResistBonus() {
+        assertEquals(1, barbarian.getResistBonus());
+    }
+}
