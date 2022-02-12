fbebea2a612d8d257ff17eaa42e50bb9d52e08ad
Vegard <vegarhgr@stud.ntnu.no>
2022-02-12 13:42:24 +0100

    The CommanderUnit and the CommanderUnitTest classes are completed

---
+++ src/main/java/no/ntnu/idatg2001/wargames/units/CommanderUnit.java
@@ -0,0 +1,39 @@
+package no.ntnu.idatg2001.wargames.units;
+
+/**
+ * A subclass of CavalryUnit. This unit, commander unit, is a better version
+ * of cavalry units due to increased damage when attacking and more armor.
+ *
+ * @author Vegard Groder
+ * @version 10.02.2022
+ */
+public class CommanderUnit extends CavalryUnit{
+
+    // Constant variables
+    private static final int ATTACK = 25;
+    private static final int ARMOR = 15;
+
+    /**
+     * Initializes all the fields in the superclass.
+     *
+     * @param name Name of the character
+     * @param health The character's health
+     * @param attack The amount of damage the character does
+     * @param armor The amount of health the character regains after being attacked
+     */
+    public CommanderUnit(String name, int health, int attack, int armor) {
+        super(name, health, attack, armor);
+    }
+
+    /**
+     * A constructor that takes in name and health as parameters
+     * and uses the constants ATTACK and ARMOR to define the amount of
+     * damage the character does, and how much armor the character has.
+     *
+     * @param name The characters name
+     * @param health The characters health
+     */
+    public CommanderUnit(String name, int health) {
+        super(name, health, ATTACK, ARMOR);
+    }
+}
---
+++ src/test/java/no/ntnu/idatg2001/wargames/units/CommanderUnitTest.java
@@ -0,0 +1,55 @@
+package no.ntnu.idatg2001.wargames.units;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommanderUnitTest {
+
+    // CommanderUnit objects made global
+    CommanderUnit archer;
+    CommanderUnit barbarian;
+
+    /**
+     * Initializes the global objects
+     */
+    @BeforeEach
+    void setUp() {
+        archer = new CommanderUnit("Archer", 20);
+        barbarian = new CommanderUnit("Barbarian", 20);
+    }
+
+    /**
+     * This test makes sure the expected attack bonus is returned
+     */
+    @Test
+    void getAttackBonus() {
+        barbarian.attack(archer);
+        assertEquals(6, barbarian.getAttackBonus());
+        barbarian.attack(archer);
+        assertEquals(2, barbarian.getAttackBonus());
+        barbarian.attack(archer);
+        assertEquals(2, barbarian.getAttackBonus());
+    }
+
+    /**
+     * Tests that the getResistBonus method returns the correct resist bonus
+     */
+    @Test
+    void getResistBonus() {
+        barbarian.attack(archer);
+        assertEquals(1, barbarian.getResistBonus());
+    }
+
+    /**
+     * Tests that the attack counter gets incremented every time
+     * a unit attacks an opponent.
+     */
+    @Test
+    void attack() {
+        assertEquals(0, barbarian.getAttackCounter());
+        barbarian.attack(archer);
+        assertEquals(1,barbarian.getAttackCounter());
+    }
+}
