394db9cf2f2e1ad22b28f52bb35e1bc6b9f9d1e5
Vegard <vegarhgr@stud.ntnu.no>
2022-03-17 15:25:15 +0100

    ArmyTest: Included tests for the four methods that retrieve the unit types in an army

--- src/test/java/no/ntnu/idatg2001/wargames/units/ArmyTest.java
+++ src/test/java/no/ntnu/idatg2001/wargames/units/ArmyTest.java
@@ -1,14 +1,16 @@
 package no.ntnu.idatg2001.wargames.units;
 
-import no.ntnu.idatg2001.wargames.units.units.CavalryUnit;
-import no.ntnu.idatg2001.wargames.units.units.CommanderUnit;
-import no.ntnu.idatg2001.wargames.units.units.Unit;
+import no.ntnu.idatg2001.wargames.units.units.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ranges.Range;
 
+import java.io.FilterOutputStream;
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +28,10 @@ class ArmyTest {
     private Unit barbarian;
     private List<Unit> units;
     private List<Unit> inputUnits;
+    List<Unit> infantryUnitList;
+    List<Unit> cavalryUnitList;
+    List<Unit> commanderUnitList;
+    List<Unit> rangedUnitList;
 
     @BeforeEach
     void setUp() {
@@ -33,10 +39,25 @@ class ArmyTest {
         army1 = new Army("Army1");
         archer = new CommanderUnit("Archer", 10);
         barbarian = new CavalryUnit("Barbarian", 12);
+        this.infantryUnitList = new ArrayList<>();
+        this.cavalryUnitList = new ArrayList<>();
+        this.commanderUnitList = new ArrayList<>();
+        this.rangedUnitList = new ArrayList<>();
         units = new ArrayList<>();
         inputUnits = new ArrayList<>();
         inputUnits.add(archer);
         inputUnits.add(barbarian);
+    }
+
+    private void addAllTypesOfUnitsToInputUnitsList() {
+        CommanderUnit mountainKing = new CommanderUnit("Mountain King", 180);
+        CavalryUnit knight = new CavalryUnit("Knight", 100);
+        InfantryUnit footman = new InfantryUnit("Footman", 100);
+        RangedUnit spearman = new RangedUnit("Spearman", 100);
+        inputUnits.add(mountainKing);
+        inputUnits.add(knight);
+        inputUnits.add(footman);
+        inputUnits.add(spearman);
     }
 
     /**
@@ -119,18 +140,88 @@ class ArmyTest {
         units.add(barbarian);
         assertEquals(2, units.size());
         int upperbound = units.size();
-        /* randomNumber is an integer between 0 and the
-           upperbound-1. The upperbound is given by the list size
-         */
+
         int randomNumber = random.nextInt(upperbound);
         units.get(randomNumber);
 
         if (randomNumber == 0) {
-            assertEquals(0, randomNumber);
             assertEquals(archer, units.get(randomNumber));
         } else if (randomNumber == 1) {
-            assertEquals(1, randomNumber);
             assertEquals(barbarian, units.get(randomNumber));
         }
     }
+
+    private List<Unit> getInfantryUnit() {
+        return inputUnits
+                .stream()
+                .filter(unit -> unit.getClass() == InfantryUnit.class)
+                .collect(Collectors.toList());
+    }
+
+    private List<Unit> getCavalryUnit() {
+        return inputUnits
+                .stream()
+                .filter(unit -> unit.getClass() == CavalryUnit.class)
+                .collect(Collectors.toList());
+    }
+
+    private List<Unit> getCommanderUnitList() {
+        return inputUnits
+                .stream()
+                .filter(unit -> unit.getClass() == CommanderUnit.class)
+                .collect(Collectors.toList());
+    }
+
+    private List<Unit> getRangedUnitList() {
+        return inputUnits
+                .stream()
+                .filter(unit -> unit.getClass() == RangedUnit.class)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    void testGetSpecificUnits() {
+        addAllTypesOfUnitsToInputUnitsList();
+        assertTrue(infantryUnitList.size() == 0);
+        assertTrue(cavalryUnitList.size() == 0);
+        assertTrue(commanderUnitList.size() == 0);
+        assertTrue(rangedUnitList.size() == 0);
+
+        this.infantryUnitList = getInfantryUnit();
+        this.cavalryUnitList = getCavalryUnit();
+        this.commanderUnitList = getCommanderUnitList();
+        this.rangedUnitList = getRangedUnitList();
+
+        assertTrue(infantryUnitList.size() != 0);
+        assertTrue(cavalryUnitList.size() != 0);
+        assertTrue(commanderUnitList.size() != 0);
+        assertTrue(rangedUnitList.size() != 0);
+
+        infantryUnitList.forEach(unit -> assertTrue(unit.getClass() == InfantryUnit.class));
+        cavalryUnitList.forEach(unit -> assertTrue(unit.getClass() ==  CavalryUnit.class));
+        commanderUnitList.forEach(unit -> assertTrue(unit.getClass() ==  CommanderUnit.class));
+        rangedUnitList.forEach(unit -> assertTrue(unit.getClass() == RangedUnit.class));
+
+        /* Tests specifically that commanderUnitList does not contain any units with
+            type CavalryUnit, since the CommanderUnit class inherits from CavalryUnit*/
+        commanderUnitList.forEach(unit -> assertFalse(unit.getClass() == CavalryUnit.class));
+
+        infantryUnitList.forEach(unit ->
+            assertFalse(unit.getClass() ==  CavalryUnit.class ||
+                    unit.getClass() == CommanderUnit.class || unit.getClass() == RangedUnit.class)
+        );
+        cavalryUnitList.forEach(unit ->
+            assertFalse(unit.getClass() == InfantryUnit.class ||
+                    unit.getClass() == CommanderUnit.class || unit.getClass() == RangedUnit.class)
+        );
+        commanderUnitList.forEach(unit ->
+            assertFalse(unit.getClass() == InfantryUnit.class ||
+                    unit.getClass() == CavalryUnit.class || unit.getClass() == RangedUnit.class)
+        );
+        rangedUnitList.forEach(unit ->
+            assertFalse(unit.getClass() == InfantryUnit.class ||
+                    unit.getClass() == CavalryUnit.class || unit.getClass() == CommanderUnit.class)
+        );
+    }
+
 }
