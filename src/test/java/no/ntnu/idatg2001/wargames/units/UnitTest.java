efc13aecdd3435189f088fe16b49a1930136da7d
Vegard <vegarhgr@stud.ntnu.no>
2022-04-08 00:47:12 +0200

    UnitTest: Removed the attack method and the getAttackBonus and getResistBonus methods because the methods is tested in its subclasses.

--- src/test/java/no/ntnu/idatg2001/wargames/units/UnitTest.java
+++ src/test/java/no/ntnu/idatg2001/wargames/units/UnitTest.java
@@ -20,28 +20,11 @@ class UnitTest {
             public int getResistBonus() {
                 return 0;
             }
+
         };
         assertEquals("Archer", unit.getName());
     }
 
-    @Test
-    void attack() {
-        Unit unit2 = new Unit("Swordsman", 120, 15, 7) {
-            @Override
-            public int getAttackBonus() {
-                return 0;
-            }
-
-            @Override
-            public int getResistBonus() {
-                return 0;
-            }
-        };
-
-        assertEquals(100, unit.getHealth());
-        unit2.attack(unit);
-        assertEquals(90, unit.getHealth());
-    }
 
     @Test
     void getName() {
@@ -70,14 +53,5 @@ class UnitTest {
         assertEquals(95, unit.getHealth());
         unit.setHealth(-100);
         assertEquals(0, unit.getHealth());
-    }
-
-    @Test
-    void getAttackBonus() {
-    }
-
-    @Test
-    void getResistBonus() {
-
     }
 }
