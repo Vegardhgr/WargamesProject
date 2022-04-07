88e9a0eb9a66777e790b4d536c271b64b98eb159
Vegardhgr <vegard.groder@gmail.com>
2022-04-08 01:28:19 +0200

    CavalryUnitTest: Included a parameterized test.

--- src/test/java/no/ntnu/idatg2001/wargames/units/CavalryUnitTest.java
+++ src/test/java/no/ntnu/idatg2001/wargames/units/CavalryUnitTest.java
@@ -1,7 +1,13 @@
 package no.ntnu.idatg2001.wargames.units;
 
+import no.ntnu.idatg2001.wargames.Battle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +25,7 @@ class CavalryUnitTest {
 
     @BeforeEach
     void setUp() {
+        Battle.setTerrain(Battle.Terrain.PLAINS);
         // Initializes the global objects
         archer = new CavalryUnit("Archer", 10, 3, 1);
         barbarian = new CavalryUnit("Barbarian", 11, 2, 2);
@@ -29,8 +36,7 @@ class CavalryUnitTest {
      */
     @Test
     void getAttackBonus() {
-        barbarian.attack(archer);
-        assertEquals(6, barbarian.getAttackBonus());
+        assertEquals(8, barbarian.getAttackBonus());
         barbarian.attack(archer);
         assertEquals(2, barbarian.getAttackBonus());
         barbarian.attack(archer);
@@ -55,6 +61,31 @@ class CavalryUnitTest {
         assertEquals(0, barbarian.getAttackCounter());
         barbarian.attack(archer);
         assertEquals(1, barbarian.getAttackCounter());
+    }
+
+    /**
+     * Tests that the correct amount of bonuses is returned
+     * depending on the terrain.
+     */
+    @ParameterizedTest
+    @MethodSource("attackAndResistBonus")
+    void testCavalryUnit(int attackBonus, int resistBonus, Battle.Terrain terrain) {
+        Battle.setTerrain(terrain);
+        CavalryUnit unit = new CavalryUnit("CavalryUnit", 10);
+        assertEquals(attackBonus, unit.getAttackBonus());
+        assertEquals(resistBonus, unit.getResistBonus());
+    }
+
+    /**
+     * Sends arguments with three parameters as a stream
+     * @return Stream<Arguments>, a stream of arguments with three parameters
+     */
+    private static Stream<Arguments> attackAndResistBonus() {
+        return Stream.of(
+                Arguments.of(2,0, Battle.Terrain.FOREST),
+                Arguments.of(2,1, Battle.Terrain.HILL),
+                Arguments.of(8,1, Battle.Terrain.PLAINS)
+        );
     }
 
     @Test
