991b7c93d1b1f60cc78df852fcbc48e3c709cacd
Vegardhgr <vegard.groder@gmail.com>
2022-04-08 17:24:13 +0200

    LoadScene: This class loads the scenes. Can only load main screen and create army scene for now.

---
+++ src/main/java/no/ntnu/idatg2001/wargames/LoadScene.java
@@ -0,0 +1,52 @@
+package no.ntnu.idatg2001.wargames;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+/**
+ * This class loads the different scenes
+ */
+public class LoadScene {
+    /**
+     * Loads the scenes
+     * @param event, the action event
+     * @param path, the path to the new scene
+     * @throws IOException, exception to be thrown if the file does not exist
+     */
+    private void loadScene(ActionEvent event, String path) throws IOException {
+        URL url = getClass().getResource(path);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(url));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Sends the main screen path to the loadScene method
+     * @param event, the action event
+     * @throws IOException, exception to be thrown if the file does not exist
+     */
+    public void loadMainScreen(ActionEvent event) throws IOException {
+        String path = "/fxmlFiles/MainScreen.fxml";
+        loadScene(event, path);
+    }
+
+    /**
+     * Sends the loadCreateArmyScene path to the loadScene method
+     * @param event, the action event
+     * @throws IOException, exception to be thrown if the file does not exist
+     */
+    public void loadCreateArmyScene(ActionEvent event) throws IOException {
+        String path = "/fxmlFiles/CreateArmy.fxml";
+        loadScene(event, path);
+    }
+}
