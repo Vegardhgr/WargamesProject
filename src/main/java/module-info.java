ec670454f4369a53374893e130766e179932224c
Vegard <vegarhgr@stud.ntnu.no>
2022-04-08 13:26:44 +0200

    module-info: Added opens to javafx.fxml and exports to the following packages: Wargames, controllers and units.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,9 @@ module no.ntnu.idatg2001.wargames {
     requires javafx.fxml;
 
     opens no.ntnu.idatg2001.wargames to javafx.fxml;
+    opens no.ntnu.idatg2001.wargames.controllers to javafx.fxml;
+    opens no.ntnu.idatg2001.wargames.units to javafx.fxml;
+    exports no.ntnu.idatg2001.wargames;
+    exports no.ntnu.idatg2001.wargames.controllers;
+    exports no.ntnu.idatg2001.wargames.units;
 }
