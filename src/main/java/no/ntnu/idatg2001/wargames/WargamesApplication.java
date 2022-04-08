d04c7dae4f8078115551be8d42bb4f5b40988ea1
Vegardhgr <vegard.groder@gmail.com>
2022-04-08 17:11:57 +0200

    WargamesApplication: Included two methods: Start and main.

---
+++ src/main/java/no/ntnu/idatg2001/wargames/WargamesApplication.java
@@ -0,0 +1,36 @@
+package no.ntnu.idatg2001.wargames;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+/**
+ * This class starts the application
+ */
+public class WargamesApplication extends Application {
+
+    /**
+     * Starts the program by opening the main screen
+     * @param stage, the stage to be shown
+     * @throws IOException, exception that will be thrown if the path does not exist
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        String path = "/fxmlFiles/MainScreen.fxml";
+        URL url = getClass().getResource(path);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(url));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
