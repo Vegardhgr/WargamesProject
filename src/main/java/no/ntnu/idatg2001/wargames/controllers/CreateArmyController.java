cc818c5d38c5f48c9915f00268bf082611e41666
Vegard <vegarhgr@stud.ntnu.no>
2022-04-08 19:07:37 +0200

    CreateArmyController: Controller for the CreateArmy.fxml class. Now possible to go back to the main screen.

---
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/CreateArmyController.java
@@ -0,0 +1,14 @@
+package no.ntnu.idatg2001.wargames.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import no.ntnu.idatg2001.wargames.SingletonClass;
+
+import java.io.IOException;
+
+public class CreateArmyController {
+    @FXML
+    private void backToMainScreen(MouseEvent event) throws IOException {
+        SingletonClass.getInstance().getScene().loadMainScreen(event);
+    }
+}
