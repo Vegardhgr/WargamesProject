8afd11058bde9cb1fa9aac4c1db8b4b5f167c9d1
Vegard <vegarhgr@stud.ntnu.no>
2022-04-08 19:02:58 +0200

    MainScreenController: Now using the class SingletonClass to load a new scene. Changed parameter from ActionEvent to MouseEvent.

--- src/main/java/no/ntnu/idatg2001/wargames/controllers/MainScreenController.java
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/MainScreenController.java
@@ -1,15 +1,14 @@
 package no.ntnu.idatg2001.wargames.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import no.ntnu.idatg2001.wargames.LoadScene;
+import javafx.scene.input.MouseEvent;
+import no.ntnu.idatg2001.wargames.SingletonClass;
 
 import java.io.IOException;
 
 public class MainScreenController {
-    LoadScene sceneLoader = new LoadScene();
     @FXML
-    private void loadCreateArmyScene(ActionEvent event) throws IOException {
-        sceneLoader.loadCreateArmyScene(event);
+    private void loadCreateArmyScene(MouseEvent event) throws IOException {
+        SingletonClass.getInstance().getScene().loadCreateArmyScene(event);
     }
 }
