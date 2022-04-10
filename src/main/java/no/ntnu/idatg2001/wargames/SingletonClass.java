6b7798273142a13d97c4b3362c637ea19c28650b
Vegard <vegarhgr@stud.ntnu.no>
2022-04-10 10:07:17 +0200

    SingletonClass: Added a FileChooser object. Also included documentation.

--- src/main/java/no/ntnu/idatg2001/wargames/SingletonClass.java
+++ src/main/java/no/ntnu/idatg2001/wargames/SingletonClass.java
@@ -1,13 +1,29 @@
 package no.ntnu.idatg2001.wargames;
 
-public class SingletonClass {
-    private static volatile SingletonClass instance;
-    private LoadScene loadScene;
+import javafx.stage.FileChooser;
 
+/**
+ * This class contains objects where only one instance
+ * of that object should be created.
+ */
+public class SingletonClass {
+    // The fields
+    private static volatile SingletonClass instance;
+    private final LoadScene loadScene;
+    private final FileChooser fileChooser;
+
+    /**
+     * The constructor. Initializes the fields.
+     */
     private SingletonClass() {
         this.loadScene = new LoadScene();
+        this.fileChooser = new FileChooser();
     }
 
+    /**
+     * Makes sure that the constructor, SingletonClass, is only called once.
+     * @return SingletonClass, an instance of this class.
+     */
     public static SingletonClass getInstance() {
         if (SingletonClass.instance == null) {
             synchronized (SingletonClass.class) {
@@ -17,7 +33,19 @@ public class SingletonClass {
         return SingletonClass.instance;
     }
 
+    /**
+     * Returns a LoadScene object.
+     * @return LoadScene, the loadScene field
+     */
     public LoadScene getScene() {
         return loadScene;
+    }
+
+    /**
+     * Returns a FileChooser object.
+     * @return FileChooser, the fileChooser field.
+     */
+    public FileChooser getFileChooser() {
+        return fileChooser;
     }
 }
