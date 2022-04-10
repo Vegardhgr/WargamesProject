c051eaaba63e50fa45debf3b37afcd1d54db7c1b
Vegard <vegarhgr@stud.ntnu.no>
2022-04-10 16:03:59 +0200

    CreateArmyController: Added a method to open the fetchArmy2Screen window.

--- src/main/java/no/ntnu/idatg2001/wargames/controllers/CreateArmyController.java
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/CreateArmyController.java
@@ -16,4 +16,9 @@ public class CreateArmyController {
     private void fetchArmy1Screen(MouseEvent event) throws IOException {
         SingletonClass.getInstance().getScene().fetchArmy1Screen(event);
     }
+
+    @FXML
+    private void fetchArmy2Screen(MouseEvent event) throws IOException {
+        SingletonClass.getInstance().getScene().fetchArmy2Screen(event);
+    }
 }
