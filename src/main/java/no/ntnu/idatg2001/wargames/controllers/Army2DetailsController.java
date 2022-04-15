0fee16ada8d3fdcd6c86e5ceebe133a2e85c7ff3
Vegardhgr <vegard.groder@gmail.com>
2022-04-15 13:32:22 +0200

    Army2DetailsController: Implemented functionality for go back button, the table view and the counter for number of units.

--- src/main/java/no/ntnu/idatg2001/wargames/controllers/Army2DetailsController.java
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/Army2DetailsController.java
@@ -1,4 +1,71 @@
 package no.ntnu.idatg2001.wargames.controllers;
 
-public class Army2DetailsController {
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import no.ntnu.idatg2001.wargames.Army;
+import no.ntnu.idatg2001.wargames.CSVFileHandler;
+import no.ntnu.idatg2001.wargames.SingletonClass;
+import no.ntnu.idatg2001.wargames.units.Unit;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Army2DetailsController implements Initializable {
+    @FXML
+    Text amountOfUnits;
+    @FXML
+    TableView<Unit> tableViewArmy2;
+    @FXML
+    TableColumn<Unit, String> unitTypeColumn;
+    @FXML
+    TableColumn<Unit, String> nameColumn;
+    @FXML
+    TableColumn<Unit, Integer> healthColumn;
+    @FXML
+    TableColumn<Unit, Integer> attackColumn;
+    @FXML
+    TableColumn<Unit, Integer> armorColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        unitTypeColumn.setCellValueFactory(new PropertyValueFactory<>("UnitType"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        healthColumn.setCellValueFactory(new PropertyValueFactory<>("Health"));
+        attackColumn.setCellValueFactory(new PropertyValueFactory<>("Attack"));
+        armorColumn.setCellValueFactory(new PropertyValueFactory<>("Armor"));
+        try {
+            amountOfUnits.setText("Total units: " + readArmy2().size());
+            unitTypeColumn.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(p.getValue()
+                    .getClass()
+                    .getSimpleName()));
+            tableViewArmy2.setItems(readArmy2());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ObservableList<Unit> readArmy2() throws IOException {
+        String pathToArmy2 = CSVFileHandler.readCSVArmyPath("src/pathToArmy2.csv");
+        if (pathToArmy2 != null) {
+            Army army = CSVFileHandler.readCSVArmy(pathToArmy2);
+            List<Unit> units = army.getUnitList();
+            return FXCollections.observableList(units);
+        }
+        return FXCollections.emptyObservableList();
+    }
+
+    @FXML
+    private void backToMainScreen(MouseEvent event) throws IOException {
+        SingletonClass.getInstance().getScene().loadMainScreen(event);
+    }
 }
