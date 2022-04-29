716da2903b133f196fc02ef977dc2ed6b4723d20
Vegardhgr <vegard.groder@gmail.com>
2022-04-29 18:13:00 +0200

    SimulateBattleController: Change the unit list to be army objects. Included a battle object.

--- src/main/java/no/ntnu/idatg2001/wargames/controllers/SimulateBattleController.java
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/SimulateBattleController.java
@@ -7,21 +7,22 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import no.ntnu.idatg2001.wargames.Army;
+import no.ntnu.idatg2001.wargames.Battle;
 import no.ntnu.idatg2001.wargames.CSVFileHandler;
 import no.ntnu.idatg2001.wargames.SingletonClass;
 import no.ntnu.idatg2001.wargames.units.Unit;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class SimulateBattleController implements Initializable {
     private static final String PATH_TO_ARMY_1 = "src/pathToArmy1.csv";
     private static final String PATH_TO_ARMY_2 = "src/pathToArmy2.csv";
-    List<Unit> unitList1;
-    List<Unit> unitList2;
-
+    private Army army1;
+    private Army army2;
+    Battle battle;
 
     @FXML
     GridPane gridPane;
@@ -35,10 +36,11 @@ public class SimulateBattleController implements Initializable {
         gridPane.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         gridPane.setGridLinesVisible(true);
         try {
-            this.unitList1 = CSVFileHandler.readCSVArmy(CSVFileHandler.readCSVArmyPath(PATH_TO_ARMY_1)).getUnitList();
-            this.unitList2 = CSVFileHandler.readCSVArmy(CSVFileHandler.readCSVArmyPath(PATH_TO_ARMY_2)).getUnitList();
+            this.army1 = CSVFileHandler.readCSVArmy(CSVFileHandler.readCSVArmyPath(PATH_TO_ARMY_1));
+            this.army2 = CSVFileHandler.readCSVArmy(CSVFileHandler.readCSVArmyPath(PATH_TO_ARMY_2));
             makeArmy1Rectangles();
             makeArmy2Rectangles();
+            battle = new Battle(army1, army2);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,7 +50,7 @@ public class SimulateBattleController implements Initializable {
     public void makeArmy1Rectangles() {
         int j = 0;
         int i = 0;
-        for (Unit unit : this.unitList1) {
+        for (Unit unit : this.army1.getUnitList()) {
             if ((i+1)%(gridPane.getRowCount()+1) == 0) {
                 j++;
                 i = 0;
@@ -66,8 +68,7 @@ public class SimulateBattleController implements Initializable {
     public void makeArmy2Rectangles() {
         int j = gridPane.getColumnCount();
         int i = 0;
-
-        for (Unit unit : this.unitList2) {
+        for (Unit unit : this.army2.getUnitList()) {
             if ((i+1)%(gridPane.getRowCount()+1) == 0) {
                 j--;
                 i = 0;
@@ -80,6 +81,4 @@ public class SimulateBattleController implements Initializable {
             i++;
         }
     }
-
-
 }
