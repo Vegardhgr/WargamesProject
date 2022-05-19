6f4aad04dd4f8e3d60d48fffa9f5f65f9aeb8fb5
Vegard <vegarhgr@stud.ntnu.no>
2022-05-19 11:35:53 +0200

    RangedUnit: Minor change in documentation.

--- src/main/java/no/ntnu/idatg2001/wargames/units/RangedUnit.java
+++ src/main/java/no/ntnu/idatg2001/wargames/units/RangedUnit.java
@@ -21,10 +21,10 @@ public class RangedUnit extends Unit {
     /**
      * Initializes all the fields in the superclass.
      *
-     * @param name   Name of the character
-     * @param health The character's health
-     * @param attack The amount of damage the character does
-     * @param armor  The amount of health the character regains after being attacked
+     * @param name, name of the character
+     * @param health, the character's health
+     * @param attack, the amount of damage the character does
+     * @param armor, the amount of health the character regains after being attacked
      */
     public RangedUnit(String name, int health, int attack, int armor) {
         super(name, health, attack, armor);
@@ -36,8 +36,8 @@ public class RangedUnit extends Unit {
      * and uses the constants ATTACK and ARMOR to define the amount of
      * damage the character does, and how much armor the character has.
      *
-     * @param name   The character's name
-     * @param health The character's health
+     * @param name, the character's name
+     * @param health, the character's health
      */
     public RangedUnit(String name, int health) {
         super(name, health, ATTACK, ARMOR);
@@ -64,7 +64,7 @@ public class RangedUnit extends Unit {
      * to its health. After the character is attacked twice,
      * it will return 2.
      *
-     * @return Int
+     * @return Int, the resist bonus
      */
     @Override
     public int getResistBonus() {
@@ -81,7 +81,7 @@ public class RangedUnit extends Unit {
      * Calls the setHealth method in the superclass unit,
      * and then increments times attacked.
      *
-     * @param health The character's new health.
+     * @param health, the character's new health.
      */
     @Override
     public void setHealth(int health) {
