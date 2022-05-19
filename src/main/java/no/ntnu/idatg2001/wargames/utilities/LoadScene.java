b178379f1d449546a4a79b9bcc2f35a4867a3e98
Vegardhgr <vegard.groder@gmail.com>
2022-05-19 18:04:28 +0200

    LoadScene: Added a try block to catch exceptions if the path to the scene does not exist.

--- src/main/java/no/ntnu/idatg2001/wargames/utilities/LoadScene.java
+++ src/main/java/no/ntnu/idatg2001/wargames/utilities/LoadScene.java
@@ -19,15 +19,20 @@ public class LoadScene {
      * Loads the scenes
      * @param event, the mouse event
      * @param path, the path to the new scene
-     * @throws IOException, exception to be thrown if the file does not exist
      */
-    private void loadScene(MouseEvent event, String path) throws IOException {
-        URL url = getClass().getResource(path);
-        Parent root = FXMLLoader.load(Objects.requireNonNull(url));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    private void loadScene(MouseEvent event, String path) {
+        try {
+            URL url = getClass().getResource(path);
+            Parent root = FXMLLoader.load(Objects.requireNonNull(url));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            Dialogs.getInstance().cannotLoadScene();
+        } catch (RuntimeException e) {
+            Dialogs.getInstance().cannotLoadScene();
+        }
     }
 
     /**
