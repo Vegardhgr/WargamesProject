8ae6f92c355a524ba1b84b0666ccc8506dad5f39
Vegardhgr <vegard.groder@gmail.com>
2022-05-20 21:19:25 +0200

    ChooseBattleType: Created both the fxml file and the controller. Added two buttons so the user can choose which simulation to run.

---
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/ChooseBattleTypeController.java
@@ -0,0 +1,41 @@
+package no.ntnu.idatg2001.wargames.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import no.ntnu.idatg2001.wargames.utilities.CSVFileHandler;
+import no.ntnu.idatg2001.wargames.utilities.Dialogs;
+import no.ntnu.idatg2001.wargames.utilities.SingletonClass;
+
+import java.io.IOException;
+
+public class ChooseBattleTypeController {
+    @FXML
+    private void animatedBattle(MouseEvent event) {
+        try {
+            int numberOfUnitsInArmy1 = CSVFileHandler.readCSVArmy(
+                            CSVFileHandler.readCSVArmyPath("src/pathToArmy1.csv"))
+                    .getUnitList().size();
+            int numberOfUnitsInArmy2 = CSVFileHandler.readCSVArmy(
+                            CSVFileHandler.readCSVArmyPath("src/pathToArmy2.csv"))
+                    .getUnitList().size();
+            if (numberOfUnitsInArmy1 > 1000 || numberOfUnitsInArmy2 > 1000) {
+                Dialogs.getInstance().tooManyUnits();
+            } else {
+                SingletonClass.getInstance().getScene().loadAnimatedBattle(event);
+            }
+        } catch (IOException e) {
+            Dialogs.getInstance().cannotLoadScene();
+        }
+    }
+
+    @FXML
+    private void quickBattle(MouseEvent event) {
+        SingletonClass.getInstance().getScene().loadQuickBattle(event);
+    }
+
+    @FXML
+    private void backToMainScreen(MouseEvent event) {
+        SingletonClass.getInstance().getScene().loadMainScreen(event);
+    }
+
+}
