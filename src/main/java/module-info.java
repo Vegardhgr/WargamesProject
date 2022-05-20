50d2e307a296024c7bcac0e30a1c4f9cca30d3a9
Vegardhgr <vegard.groder@gmail.com>
2022-05-20 21:16:09 +0200

    module-info: Small change.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,14 @@ module no.ntnu.idatg2001.wargames {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens no.ntnu.idatg2001.wargames to javafx.fxml;
-    opens no.ntnu.idatg2001.wargames.controllers to javafx.fxml;
-    opens no.ntnu.idatg2001.wargames.units to javafx.fxml;
+    opens no.ntnu.idatg2001.wargames;
+    opens no.ntnu.idatg2001.wargames.controllers;
+    opens no.ntnu.idatg2001.wargames.units;
     exports no.ntnu.idatg2001.wargames;
     exports no.ntnu.idatg2001.wargames.controllers;
     exports no.ntnu.idatg2001.wargames.units;
     exports no.ntnu.idatg2001.wargames.utilities;
-    opens no.ntnu.idatg2001.wargames.utilities to javafx.fxml;
+    opens no.ntnu.idatg2001.wargames.utilities;
     exports no.ntnu.idatg2001.wargames.app;
-    opens no.ntnu.idatg2001.wargames.app to javafx.fxml;
+    opens no.ntnu.idatg2001.wargames.app;
 }
