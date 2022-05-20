6be1989c48a57c4859abbc6ec0fa05100ea439f9
Vegardhgr <vegard.groder@gmail.com>
2022-05-20 21:35:27 +0200

    MainScreen: Change the path of the scene that shows up when the button simulate battle is clicked. Also made the main screen bigger.

--- src/main/java/no/ntnu/idatg2001/wargames/controllers/MainScreenController.java
+++ src/main/java/no/ntnu/idatg2001/wargames/controllers/MainScreenController.java
@@ -4,21 +4,19 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import no.ntnu.idatg2001.wargames.utilities.SingletonClass;
 
-import java.io.IOException;
-
 public class MainScreenController {
     @FXML
-    private void simulateBattle(MouseEvent event) throws IOException {
-        SingletonClass.getInstance().getScene().loadSimulateBattle(event);
+    private void simulateBattle(MouseEvent event) {
+        SingletonClass.getInstance().getScene().loadChooseBattleType(event);
     }
 
     @FXML
-    private void editArmy(MouseEvent event) throws IOException {
+    private void editArmy(MouseEvent event) {
         SingletonClass.getInstance().getScene().loadEditArmy(event);
     }
 
     @FXML
-    private void loadCreateArmyScene(MouseEvent event) throws IOException {
+    private void loadCreateArmyScene(MouseEvent event) {
         SingletonClass.getInstance().getScene().loadCreateArmyScene(event);
     }
 }
