1879313c01b4e5343317adeb6982f3e5f5e68c17
Vegard <vegarhgr@stud.ntnu.no>
2022-05-21 20:47:48 +0200

    module-info: Changed because of refactored files.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,19 @@ module no.ntnu.idatg2001.wargames {
     requires javafx.fxml;
 
     opens no.ntnu.idatg2001.wargames;
-    opens no.ntnu.idatg2001.wargames.controllers;
     opens no.ntnu.idatg2001.wargames.units;
     exports no.ntnu.idatg2001.wargames;
-    exports no.ntnu.idatg2001.wargames.controllers;
     exports no.ntnu.idatg2001.wargames.units;
     exports no.ntnu.idatg2001.wargames.utilities;
     opens no.ntnu.idatg2001.wargames.utilities;
     exports no.ntnu.idatg2001.wargames.app;
     opens no.ntnu.idatg2001.wargames.app;
+    exports no.ntnu.idatg2001.wargames.controllers.createArmy;
+    opens no.ntnu.idatg2001.wargames.controllers.createArmy;
+    exports no.ntnu.idatg2001.wargames.controllers.mainScreen;
+    opens no.ntnu.idatg2001.wargames.controllers.mainScreen;
+    exports no.ntnu.idatg2001.wargames.controllers.simulateBattle;
+    opens no.ntnu.idatg2001.wargames.controllers.simulateBattle;
+    exports no.ntnu.idatg2001.wargames.controllers.viewArmyDetails;
+    opens no.ntnu.idatg2001.wargames.controllers.viewArmyDetails;
 }
