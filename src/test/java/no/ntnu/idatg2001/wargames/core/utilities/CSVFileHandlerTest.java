d06bb5e5ce130879f398e4544b8246abada3d9b1
Vegard <vegarhgr@stud.ntnu.no>
2022-05-23 13:29:15 +0200

    CSVFileHandlerTest: Included try-catch blocks.

--- src/test/java/no/ntnu/idatg2001/wargames/core/utilities/CSVFileHandlerTest.java
+++ src/test/java/no/ntnu/idatg2001/wargames/core/utilities/CSVFileHandlerTest.java
@@ -8,13 +8,18 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CSVFileHandlerTest {
     @Test
-    void readCSVTest() throws IOException {
-        Army army = CSVFileHandler.readCSVArmy("src/humanArmyTestFile.csv");
-        Assertions.assertEquals("Human Army", army.getName());
-        Assertions.assertEquals(14, army.getUnitList().size());
+    void readCSVTest() {
+        try {
+            Army army = CSVFileHandler.readCSVArmy("files/testFiles/humanArmyTestFile.csv");
+            Assertions.assertEquals("Human Army", army.getName());
+            Assertions.assertEquals(13, army.getUnitList().size());
+        } catch (IOException e) {
+            fail("IOException because file does not exist.");
+        }
     }
 
     @Test
@@ -24,19 +29,23 @@ class CSVFileHandlerTest {
     }
 
     @Test
-    void writeCSVTest() throws IOException {
-        String filename = "src/orcishHordeArmyTestFile.csv";
-        List<Unit> units = new ArrayList<>();
-        CSVFileHandler.writeCSVArmy(new Army("", units), filename);
-        Assertions.assertEquals("", CSVFileHandler.readCSVArmy(filename).getName());
-        Assertions.assertEquals(0, CSVFileHandler.readCSVArmy(filename).getUnitList().size());
-        units.add(new CommanderUnit("Mountain King", 180));
-        units.add(new InfantryUnit("Footman", 100));
-        units.add(new CavalryUnit("Knight", 100));
-        units.add(new RangedUnit("Archer", 100));
-        CSVFileHandler.writeCSVArmy(new Army("Orcish Horde Army", units), filename);
-        Army army = CSVFileHandler.readCSVArmy(filename);
-        Assertions.assertEquals("Orcish Horde Army", army.getName());
-        Assertions.assertEquals(4, army.getUnitList().size());
+    void writeCSVTest() {
+        try {
+            String filename = "files/testFiles/orcishHordeArmyTestFile.csv";
+            List<Unit> units = new ArrayList<>();
+            CSVFileHandler.writeCSVArmy(new Army("", units), filename);
+            Assertions.assertEquals("", CSVFileHandler.readCSVArmy(filename).getName());
+            Assertions.assertEquals(0, CSVFileHandler.readCSVArmy(filename).getUnitList().size());
+            units.add(new CommanderUnit("Mountain King", 180));
+            units.add(new InfantryUnit("Footman", 100));
+            units.add(new CavalryUnit("Knight", 100));
+            units.add(new RangedUnit("Archer", 100));
+            CSVFileHandler.writeCSVArmy(new Army("Orcish Horde Army", units), filename);
+            Army army = CSVFileHandler.readCSVArmy(filename);
+            Assertions.assertEquals("Orcish Horde Army", army.getName());
+            Assertions.assertEquals(4, army.getUnitList().size());
+        } catch (IOException e) {
+            fail("IOException because file does not exist.");
+        }
     }
 }
