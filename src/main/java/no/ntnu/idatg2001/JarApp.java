9e45099566d5d3851a9081b116948680459696db
Vegardhgr <vegard.groder@gmail.com>
2022-05-23 14:42:16 +0200

    JarApp: Included a class that calls the main class in order to create the shaded jar file.

---
+++ src/main/java/no/ntnu/idatg2001/JarApp.java
@@ -0,0 +1,22 @@
+package no.ntnu.idatg2001;
+
+import no.ntnu.idatg2001.wargames.app.WargamesApplication;
+
+/**
+ * This class calls the main method in WargamesApplication.
+ *
+ * @author Vegard Grøder
+ * @version 1.0.0
+ */
+public class JarApp {
+
+    /**
+     * This main method calls the main method in
+     * WargamesApplication
+     *
+     * @param args of type String.
+     */
+    public static void main(String[] args) {
+        WargamesApplication.main(args);
+    }
+}
