506c0b9e7ec8b8e2e7a1224f3e1051e4a2c0ca1d
Vegardhgr <vegard.groder@gmail.com>
2022-05-23 17:08:31 +0200

    Checker for if the files needed exists. Else they will be created.

--- src/main/java/no/ntnu/idatg2001/wargames/app/WargamesApplication.java
+++ src/main/java/no/ntnu/idatg2001/wargames/app/WargamesApplication.java
@@ -5,9 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import no.ntnu.idatg2001.wargames.core.Army;
+import no.ntnu.idatg2001.wargames.core.utilities.CSVFileHandler;
+import no.ntnu.idatg2001.wargames.core.utilities.Dialogs;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.Objects;
 
 /**
@@ -22,6 +28,25 @@ public class WargamesApplication extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
+        String filename1 = "pathToArmy1.csv";
+        String filename2 = "pathToArmy2.csv";
+        String savePath1 = Path.of(filename1).toAbsolutePath().toString();
+        String savePath2 = Path.of(filename2).toAbsolutePath().toString();
+
+        if (!fileExists(savePath1)) {
+            try {
+                CSVFileHandler.writeCSVArmy(new Army("", Collections.emptyList()), filename1);
+            } catch (IOException e) {
+                Dialogs.getInstance().somethingWrongWithTheFile();
+            }
+        }
+        if (!fileExists(savePath2)) {
+            try {
+                CSVFileHandler.writeCSVArmy(new Army("", Collections.emptyList()), filename2);
+            } catch (IOException e) {
+                Dialogs.getInstance().somethingWrongWithTheFile();
+            }
+        }
         String path = "/fxmlFiles/mainScreen/MainScreen.fxml";
         URL url = getClass().getResource(path);
         Parent root = FXMLLoader.load(Objects.requireNonNull(url));
@@ -29,6 +54,11 @@ public class WargamesApplication extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+    }
+
+    private boolean fileExists(String filePath) {
+        File file = new File(filePath);
+        return (file.exists() && !file.isDirectory());
     }
 
     /**
